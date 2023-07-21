6d3480923662c1735d581aebe5ff9361597f7268
smitFromWolkus <smit.gor@wolkus.com>
2023-07-21 10:40:49 +0530

    vite + vue2

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+import vue from "@vitejs/plugin-vue2";
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [vue()],
+// })
+
+export default {
+  plugins: [vue()],
+};
